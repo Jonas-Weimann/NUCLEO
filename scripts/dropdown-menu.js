175336c81584cdc54c40f7db74db6f55fe9b6a39
Jonas <jonas.weimann04@gmail.com>
2024-07-03 03:14:23 -0300

    Animando el dropdown menu

--- scripts/dropdown-menu.js
+++ scripts/dropdown-menu.js
@@ -1,11 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-const bar1 = document.querySelector(".bar-1")
-const bar3 = document.querySelector(".bar-3")
+const navMenuUl = document.querySelector(".navegacion");
+const bar1 = document.querySelector(".bar-1");
+const bar3 = document.querySelector(".bar-3");
+const botonera = document.querySelector(".botones");
 
 hamburger.addEventListener("click", ()=> {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    navMenuUl.classList.toggle("active");
+    botonera.classList.toggle("active");
+    navMenuUl.appendChild(botonera);
     bar1.classList.add("animation");
     bar3.classList.add("animation");
 })
